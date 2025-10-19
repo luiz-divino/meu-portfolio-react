ba4443d04d22d2c53490a41afb3b34219b25037a
unknown <nandodivinodev@gmail.com>
2025-10-19 01:09:43 -0300

    responsavel por aplicar os estilos globais(como fontes e resete inicial do projeto) com styled-components

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+*{
+margin:0;
+paddin:0;
+box-sizing:border-box;
+}
+
+body{
+    background:#121212; 
+    color: #01DF76; // Cor padrão do texto
+    font-family: 'IBM Plex Sans', sans-serif;
+}
+
+
+
+
+
+
+`
